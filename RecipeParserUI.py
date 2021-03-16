55ee030c7135a8b499d74056fde03fae5c655eec
Alex <alexli2021@u.northwestern.edu>
2021-03-15 19:09:26 -0500

    parser UI changes

--- RecipeParserUI.py
+++ RecipeParserUI.py
@@ -2,16 +2,22 @@ import sys
 import HTMLParser
 import InstructionParser
 import Transformation
+import time
 
-def urlinput():
-    url = input("Enter recipe url or type 'quit' to exit recipe parser: \n")
+def urlinput(first):
+    if first: #detects if it's the first time looping
+        url = input("Hi, my name is Eugene, and I'll be your virtual assistant today. Please enter an All-Recipe url to get started or type 'quit' to exit. \n")
+    else:
+        url = input("Sure, let's look at another recipe. Please enter an All-Recipe url or type 'quit' to exit.\n")
     valid = False
     while valid is False:
         if url == "quit":
+            print("Goodbye!")
+            time.sleep(2) #pause to let the print message display before quitting
             sys.exit(0)
         results = HTMLParser.fetchAndParseHTML(url)
         if results == {}:
-            url = input("Invalid. Please try another url or type 'quit' to exit recipe parser: \n")
+            url = input("I'm sorry, it looks like the url is invalid. Please try another url or type 'quit' to exit recipe parser: \n")
         else:
             valid = True
     parserUI(results)
@@ -19,74 +25,37 @@ def parserUI(results):
     ingredients = HTMLParser.format_ings(HTMLParser.get_ingredients(results["ingredients"]))
     tools_instructions = InstructionParser.parseToolsAndCookingMethod(results)
     active = True
-    while active:
-        user = input("Enter corresponding number for desired info:\n1:Ingredients and Steps\n2:Tools\n3:Cooking Methods\n4:Transform to vegetarian\n5:Transform from vegetarian\n6:Transform to healthy\n7:Transform to unhealthy\n8:Transform to Italian-style\n9:Enter a new recipe\n'quit':Quit\n")
-        if int(user) == 1:
-            print_ing = "Ingredients:\n"
+    first = True
+    while active is True:
+        if first:
+            user = input("I've found the recipe for \"" + results["name"] + "\". Would you like to: [1] look at the ingredients, [2] look at the steps, [3] enter another recipe, or [4] quit the conversation? \n")
+        else:
+            user = input("I'm sorry, I couldn't process that input. Would you like to: [1] look at the ingredients, [2] look at the steps, [3] enter another recipe, or [4] quit the conversation? \n")
+        if user == '1':
+            print_ing = "Here is the list of ingredients:\n"
             for ing in ingredients:
                 print_ing += ing + "\n"
-            print_steps = "Steps:\n"
-            for step in tools_instructions['steps']:
-                print_steps += step['instruction'] + "\n"
-            print(print_ing+print_steps)
-        elif int(user) == 2:
-            print_tools = "Tools:\n"
-            for tools in tools_instructions['tools']:
-                print_tools += tools + "\n"
-            print(print_tools)
-        elif int(user) == 3:
-            print_methods = "Methods:\n"
-            print_methods += tools_instructions['main_cooking_method'] + ' (main), '
-            print_methods += tools_instructions['secondary_cooking_method'] + ' (secondary)\n'
-            print(print_methods)
-        elif int(user) == 4:
-            if HTMLParser.to_vegetarian(HTMLParser.get_ingredients(results["ingredients"])) == {}:
-                print("Recipe is already vegetarian!")
-            else:
-                to_veg = Transformation.toVeggie(tools_instructions)
-                new_ing = HTMLParser.format_ings(to_veg['ingredients'])
-                print_ing = "Ingredients:\n"
-                for ing in new_ing:
-                    print_ing += ing + "\n"
-                print(print_ing)
-        elif int(user) == 5:
-            if HTMLParser.from_vegetarian(HTMLParser.get_ingredients(results["ingredients"])) == {}:
-                print("Recipe isn't vegetarian!")
-            else:
-                from_veg = Transformation.fromVeggie(tools_instructions)
-                new_ing = HTMLParser.format_ings(from_veg['ingredients'])
-                print_ing = "Ingredients:\n"
-                for ing in new_ing:
-                    print_ing += ing + "\n"
-                print(print_ing)
-        elif int(user) == 6:
-            healthy = HTMLParser.to_healthy(HTMLParser.get_ingredients(results["ingredients"]))
-            healthy = HTMLParser.format_ings(healthy)
-            print_ing = "Ingredients:\n"
-            for ing in healthy:
-                print_ing += ing + "\n"
             print(print_ing)
-        elif int(user) == 7:
-            healthy = HTMLParser.to_unhealthy(HTMLParser.get_ingredients(results["ingredients"]))
-            healthy = HTMLParser.format_ings(healthy)
-            print_ing = "Ingredients:\n"
-            for ing in healthy:
-                print_ing += ing + "\n"
-            print(print_ing)
-        elif int(user) == 8:
-            italian = Transformation.toItalian(results)
-            res = HTMLParser.format_ings(italian['ingredients'])
-            print_ing = "Ingredients:\n"
-            for ing in res:
-                print_ing += ing + "\n"
-            print(print_ing)
-        elif int(user) == 9:
-            urlinput()
+            conversation(results)
+            active = False
+        elif user == '2':
+            print("The first step is: " + tools_instructions["steps"][0]["instruction"] + "\n")
+            conversation(results)
+            active = False
+        elif user == '3':
+            urlinput(False)
+            active = False
+        elif user == '4':
+            print("Goodbye!")
+            time.sleep(2)
+            sys.exit(0)
         else:
-            print("Hello")
-    print(print_ing)
+            first = False
+def conversation(results):
+    print("What next?")
+    #todo: implement conversation parser that will start after either ingredients or the first step is requested            
 
     
 
 if __name__ == '__main__':
-    urlinput()
+    urlinput(True)
